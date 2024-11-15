90e1a8805359ed1baa9f8cac82cb3d3dbfc30373
codeza-ai <darshan.odedara@gmail.com>
2024-11-15 22:51:39 +0530

    feat: Add edit option in table

--- backend/getData.js
+++ backend/getData.js
@@ -25,6 +25,7 @@ async function getTableData(req, res) {
 }
 
 async function postBillData(req, res) {
+    const edit = req.query.edit;
     const {
         Payment_ID,
         Date,
@@ -34,7 +35,24 @@ async function postBillData(req, res) {
         M_Cost,
         Total,
         Patient_ID
-     } = req.body.params.billData;
+    } = req.body.params.billData;
+    if(edit === "true"){
+        try{
+            await db.query(`update bill set 
+                Date = '${Date}',
+                Room_Cost = ${Room_Cost},
+                Test_Cost = ${Test_Cost},
+                Other_Charges = ${Other_Charges},
+                M_Cost = ${M_Cost},
+                Total = ${Total},
+                Patient_ID = ${Patient_ID}
+                where Payment_ID = ${Payment_ID};`);
+        }catch(e){
+            console.log(e);
+        }
+        res.json({msg:"Successfully edited"});
+        return;
+    }
 
     try{
         await db.query(`insert into bill values(
@@ -53,6 +71,40 @@ async function postBillData(req, res) {
 
     res.json({msg:"Success"});
 }
+
+async function postPatientData(req, res){
+    const {
+        Patient_ID,
+        Patient_Fname,
+        Patient_Lname,
+        Blood_type,
+        Email,
+        Gender,
+        Condition,
+        Admission_Date,
+        Discharge_Date,
+        Phone
+    } = req.body.params.patientData;
+
+    try {
+        await db.query(`insert into patient values(
+            ${Patient_ID},
+            '${Patient_Fname}',
+            '${Patient_Lname}',
+            '${Blood_type}',
+            '${Email}',
+            '${Gender}',
+            '${Condition}',
+            '${Admission_Date}',
+            '${Discharge_Date}',
+            '${Phone}'
+        );`);
+    } catch (e) {
+        console.log(e);
+    }
+
+    res.json({msg: "Success"});
+}
 async function deleteData(req, res) {
     const tableName = req.query.tableName;
     const primaryKey = req.query.primaryKey;
@@ -68,4 +120,4 @@ async function deleteData(req, res) {
 
 
 
-export { getTableData , postBillData, deleteData};
+export { getTableData, postBillData, deleteData, postPatientData };
--- frontend/src/Components/Table.jsx
+++ frontend/src/Components/Table.jsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 
-const Table = ({ columns, data, deleteFunction }) => {
+const Table = ({ columns, data, deleteFunction , editFunction}) => {
     return (
         <div className="overflow-x-auto">
             <table className="min-w-full border border-gray-200 rounded-xl overflow-hidden">
                 <thead>
                     <tr className="bg-gray-800 border-b">
                         {columns.map((column, index) => (
-                            <th key={index} className="px-4 py-2 text-left text-blue-500">
+                            <th key={index} className="px-4 py-2 text-center text-blue-500">
                                 {column}
                             </th>
                         ))}
@@ -20,14 +20,21 @@ const Table = ({ columns, data, deleteFunction }) => {
                     {data.map((row, rowIndex) => (
                         <tr key={rowIndex} className="border-b hover:bg-gray-200 bg-gray-50">
                             {row.map((cell, cellIndex) => (
-                                <td key={cellIndex} className="px-4 py-2">
+                                <td key={cellIndex} className="px-4 py-2 text-center">
                                     {cell}
                                 </td>
                             ))}
                             <td>
-                                <button className="text-red-500" onClick={()=> {
-                                    deleteFunction(row[0]);
-                                }}>Delete</button>
+                                <div className='flex gap-3 p-2'>
+                                    <button className="text-red-500" onClick={() => {
+                                        deleteFunction(row[0]);
+                                    }}>Delete</button>
+                                    <button className="text-blue-500"
+                                        onClick={() => {
+                                            editFunction(row, true);
+                                        }}
+                                    >Edit</button>
+                                </div>
                             </td>
                         </tr>
                     ))}
@@ -39,7 +46,8 @@ const Table = ({ columns, data, deleteFunction }) => {
 Table.propTypes = {
     columns: PropTypes.arrayOf(PropTypes.string).isRequired,
     data: PropTypes.arrayOf(PropTypes.array).isRequired,
-    deleteFunction: PropTypes.func.isRequired
+    deleteFunction: PropTypes.func.isRequired,
+    editFunction: PropTypes.func.isRequired
 };
 
 export default Table;
